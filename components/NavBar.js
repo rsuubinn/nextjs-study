44ce1c9c3988597217c1ec8ac15cfe1a545ae2bc
rsuubinn <rsuubinn@gmail.com>
2022-11-05 18:26:54 +0900

    #1.4 CSS Modules

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,19 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
+  const router = useRouter();
   return (
     <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
+      <Link href="/" legacyBehavior>
+        <a className={`${router.pathname === "/" ? styles.active : ""}`}>
+          Home
+        </a>
+      </Link>
+      <Link href="/about" legacyBehavior>
+        <a>About</a>
+      </Link>
     </nav>
   );
 }
