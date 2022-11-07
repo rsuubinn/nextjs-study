76a1c9a5640223d5e41539d3ae9a1190556fa14c
rsuubinn <rsuubinn@gmail.com>
2022-11-07 23:38:54 +0900

    #2.0 Patterns

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
 
 export default function Home() {
   return (
     <div>
+      <Seo title="Home" />
       <h1>hi</h1>
     </div>
   );
