0f6ef8d9f2599e270fa16a80e49c39d911fbc733
rsuubinn <rsuubinn@gmail.com>
2022-11-08 14:57:32 +0900

    #2.1 Fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
-import NavBar from "../components/NavBar";
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "9c9934e5f1d2067a566e93eede75e49e";
+
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title="Home" />
-      <h1>hi</h1>
+      <h1>Home</h1>
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
