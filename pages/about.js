9a609e95175c56c39ae8978a7e098ae7caa49902
rsuubinn <rsuubinn@gmail.com>
2023-05-08 02:57:58 +0900

    feat: 홈 화면 구성

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Seo from "../components/Seo";
+
+const About = () => {
+  return (
+    <div>
+      <Seo title="About" />
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
